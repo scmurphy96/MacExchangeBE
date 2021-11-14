81895891885ed52297a0a21da6a40d7bfdc2ef48
Sean Murphy <scmurphy96@gmail.com>
2021-11-14 18:52:23 -0500

    Added first component

---
+++ src/components/Form.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import TextField from '@mui/material/TextField';
+
+const Form = (props) => (
+  <div>
+    <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+  </div>
+);
+
+export default Form;
